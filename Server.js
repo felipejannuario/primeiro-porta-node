2ddf979bf46dde005718148d682cff47b2798f7d
Felipe Januario <felipejanuarioalves@outlook.com>
2025-08-21 21:38:44 -0300

    ok

--- Server.js
+++ Server.js
@@ -1,5 +1,19 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.listen(3000)
+const users = [];
+
+app.get('/', (req, res) => {
+    res.send('<h1>API está rodando! Use /usuarios para listar os usuários.</h1>');
+});
+
+app.post('/usuarios', (req, res) => {
+    users.push(req.body);
+    res.status(201).json({ message: "usuário criado com sucesso" });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+});
