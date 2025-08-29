719813b473f7e8b941b7c8752e87fddd63e8a56d
Felipe Januario <felipejanuarioalves@outlook.com>
2025-08-28 21:46:22 -0300

    feat: atualiza versão do Prisma para 6.15.0 e adiciona arquivos de migração

--- Server.js
+++ Server.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());  //aqui habilito o http://localhost:3000 ou o servidor que for rodar pra abrir o unico client
 
 const users = [];
 
-app.get('/', (req, res) => {
+app.get('/usuarios', (req, res) => {
     res.send('API está rodando! Use /usuarios para listar os usuários.');
 });
 
