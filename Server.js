bacd7cde8516e1420461d056a2138d088bd2d94f
Felipe Januario <felipejanuarioalves@outlook.com>
2025-09-02 00:22:32 -0300

    refactor: ajusta formatação e melhora a mensagem da rota GET /usuarios

--- Server.js
+++ Server.js
@@ -1,25 +1,24 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors());  //aqui habilito o http://localhost:3000 ou o servidor que for rodar pra abrir o unico client
+app.use(cors()); //aqui habilito o http://localhost:3000 ou o servidor que for rodar pra abrir o unico client
 
 const users = [];
 
-app.get('/usuarios', (req, res) => {
-   res.json({
-        message: 'API está rodando! Aqui está a lista de usuários:',
-        
-        users: users
-    });
+app.get("/usuarios", (req, res) => {
+  res.json({
+    message: "API está rodando!",
+    users: users, // sem colchetes extras
+  });
 });
 
-app.post('/usuarios', (req, res) => {
-    users.push(req.body);
-    res.status(201).json({ message: "usuário criado com sucesso" });
+app.post("/usuarios", (req, res) => {
+  users.push(req.body);
+  res.status(201).json({ message: "usuário criado com sucesso" });
 });
 
 app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000');
+  console.log("Servidor rodando em http://localhost:3000");
 });
